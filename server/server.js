98f7eebfcd8c17ec536e5717eaadae53a305d5ba
Mayank Panwar <mayankpanwar066@gmail.com>
2025-05-31 11:51:21 +0530

    fix cors error

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 // middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-ashy-three.vercel.app",
+    ],
     credentials: true,
   })
 );
